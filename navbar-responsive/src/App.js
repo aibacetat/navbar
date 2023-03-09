a12d8a4a1171c4d957b29092071391329ff784ed
aibacetat <115603178+aibacetat@users.noreply.github.com>
2023-03-09 20:00:36 -0300

    itemlistcontainer
    itemlistcontainer commit

--- navbar-responsive/src/App.js
+++ navbar-responsive/src/App.js
@@ -1,9 +1,12 @@
 import Navbar from "./components/Navbar/Navbar";
+import ItemList from "./components/Navbar/ItemListContainer";
 
 function App() {
   return (
     <>
       <Navbar />
+      
+      <ItemList />
     </>
   );
 }
